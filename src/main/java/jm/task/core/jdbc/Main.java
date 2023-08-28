1c81242c9ac750c40e1a58fbf740beb585227aae
ErikMarx <121640897+MarxErik@users.noreply.github.com>
2023-08-29 00:33:56 +0600

    Revert "Initial commit"
    This reverts commit 2c048dffcff1120b505d83e62c8b3672c1155f8c.
    
    # Conflicts:
    #	pom.xml
    #	src/main/java/jm/task/core/jdbc/Main.java
    #	src/main/java/jm/task/core/jdbc/dao/UserDao.java
    #	src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
    #	src/main/java/jm/task/core/jdbc/model/User.java
    #	src/main/java/jm/task/core/jdbc/service/UserService.java
    #	src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
    #	src/main/java/jm/task/core/jdbc/util/Util.java
    #	src/test/java/UserServiceTest.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,23 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
-
-import java.sql.SQLException;
-
 public class Main {
-    private final static UserService userService = new UserServiceImpl();
-    public static void main(String[] args) throws SQLException {
-//        userService.createUsersTable();
-//        userService.saveUser("Shaha", "Ivanov", (byte) 23);
-//        userService.saveUser("Shahasdaa", "Ivaasdanov", (byte) 43);
-//        //userService.dropUsersTable();
-//        userService.removeUserById(2);
-//        System.out.println(userService.getAllUsers());
-//        userService.cleanUsersTable();
-//        System.out.println(userService.getAllUsers());
-        userService.dropUsersTable();
-        userService.createUsersTable();
-        userService.saveUser("ShahaSDASDsdaa", "ASFSAFIvaasdanov", (byte) 63);
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
     }
 }
